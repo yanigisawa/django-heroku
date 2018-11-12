9dd1c4eab7bba905a816aebcd6c94315e645f648
James Alexander <jalexander@leafsoftwaresolutions.com>
2018-11-12 09:39:56 -0500

    - Mostly finish final Django Tutorial - Add Bootstrap CSS - Add Transparent repeating Background

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-
+from django.utils import timezone
 from polls.models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
