1f5773860528874ed956404b1712fbdec485ad0c
James Alexander <jalexander@leafsoftwaresolutions.com>
2018-11-13 16:18:59 -0500

    - Update prod database connection settings

--- mysite/prod.py
+++ mysite/prod.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,11 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': os.environ.get('DATABASE_URL', default='sqlite3:///db.sqlite3')
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=500)
 }
 
 
